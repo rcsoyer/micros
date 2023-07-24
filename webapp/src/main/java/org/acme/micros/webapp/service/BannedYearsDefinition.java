c868dafa192c50d821960bbb3f43d97836eaf07b
Rafa Soyer <rcsoyer@gmail.com>
2023-07-24 13:36:46 +0200

    simple refactor only to test the github pipelines

--- webapp/src/main/java/org/acme/micros/webapp/service/BannedYearsDefinition.java
+++ webapp/src/main/java/org/acme/micros/webapp/service/BannedYearsDefinition.java
@@ -1,20 +1,18 @@
 package org.acme.micros.webapp.service;
 
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.UncheckedIOException;
+import java.nio.file.Files;
 import java.time.Year;
 import java.util.Set;
 
 import lombok.extern.slf4j.Slf4j;
 import org.acme.micros.webapp.domain.Person;
-import org.apache.commons.io.IOUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Component;
 
-import static java.nio.charset.Charset.defaultCharset;
 import static java.util.stream.Collectors.toUnmodifiableSet;
 import static org.acme.micros.webapp.configuration.CachingConfig.CACHE_PEOPLE_BANNED_YEARS;
 
@@ -42,13 +40,11 @@ public class BannedYearsDefinition {
     public Set<Integer> bannedYearsFromFile() {
         log.debug("Load the banned years info into the cache");
 
-        try (final InputStream fileLines = bannedYearsFile.getInputStream()) {
-            return IOUtils
-              .readLines(fileLines, defaultCharset())
-              .stream()
-              .map(Year::parse)
-              .map(Year::getValue)
-              .collect(toUnmodifiableSet());
+        try (final var fileLines = Files.lines(bannedYearsFile.getFile().toPath())) {
+            return fileLines
+                     .map(Year::parse)
+                     .map(Year::getValue)
+                     .collect(toUnmodifiableSet());
         } catch (IOException ioException) {
             log.error("Failure while reading the file with the people's banned years");
             throw new UncheckedIOException("Internal app error", ioException);
