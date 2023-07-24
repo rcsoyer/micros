d4850a1291cfed06178797cf22ebe666f53c1674
Rafa Soyer <rcsoyer@gmail.com>
2023-07-24 12:40:53 +0200

    adjust the security context config as auditing of the actions over the data in the system

--- webapp/src/main/java/org/acme/micros/webapp/configuration/AuditConfig.java
+++ webapp/src/main/java/org/acme/micros/webapp/configuration/AuditConfig.java
@@ -1,7 +1,13 @@
 package org.acme.micros.webapp.configuration;
 
+import java.util.Optional;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 
 /**
  * Configuration for auditing JPA entities
@@ -10,12 +16,14 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @EnableJpaAuditing
 public class AuditConfig {
 
-/*    *
-     The user that is logged in the app when creating or modifying resources
+    /***
+     * The user that is logged in the app when creating or modifying resources
+     */
     @Bean
     public AuditorAware<String> auditorProvider() {
-        return () -> Optional.ofNullable(SecurityContextHolder.getContext().getAuthentication())
-                             .map(Authentication::getName);
-    }*/
+        return () -> Optional
+                       .ofNullable(SecurityContextHolder.getContext().getAuthentication())
+                       .map(Authentication::getName);
+    }
 
 }
