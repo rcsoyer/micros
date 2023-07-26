a9e56fc0b1202a5dc9003c67a7e09b7ab2f52152
Rafa Soyer <rcsoyer@gmail.com>
2023-07-26 12:59:23 +0200

    disable the security part and keep it for future references. To properly configure the security context would demand some more configuration

--- webapp/src/main/java/org/acme/micros/webapp/configuration/AuditConfig.java
+++ webapp/src/main/java/org/acme/micros/webapp/configuration/AuditConfig.java
@@ -1,24 +1,16 @@
 package org.acme.micros.webapp.configuration;
 
-import java.util.Optional;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.domain.AuditorAware;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-
+//its useful for reference. But in here disabling it since it would demand to configure a properly secure access
 /**
  * Configuration for auditing JPA entities
  */
-@Configuration
+/*@Configuration
 @EnableJpaAuditing
 public class AuditConfig {
 
-    /***
+    *//***
      * The user that is logged in the app when creating or modifying resources
-     */
+     *//*
     @Bean
     public AuditorAware<String> auditorProvider() {
         return () -> Optional
@@ -26,4 +18,4 @@ public class AuditConfig {
                        .map(Authentication::getName);
     }
 
-}
+}*/
