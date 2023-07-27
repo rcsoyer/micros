620192ccca5c30ae2304b8cb3fef12c44516329b
Rafa Soyer <rcsoyer@gmail.com>
2023-07-27 15:39:33 +0200

    simple formatting

--- webapp/src/main/java/org/acme/micros/webapp/repository/PersonRepository.java
+++ webapp/src/main/java/org/acme/micros/webapp/repository/PersonRepository.java
@@ -15,8 +15,8 @@ public interface PersonRepository extends JpaRepository<Person, Long> {
     boolean existsByName(String name);
 
     @Query("SELECT p.id AS id, p.name AS name, p.birthDate AS birthDate, " +
-               "p.dateCreated AS dateCreated, p.dateModified AS dateModified " +
-               "FROM Person p " +
-               "WHERE YEAR(p.birthDate) NOT IN (?1)")
+             "p.dateCreated AS dateCreated, p.dateModified AS dateModified " +
+             "FROM Person p " +
+             "WHERE YEAR(p.birthDate) NOT IN (?1)")
     List<PersonFiltered> findByNotIn(Set<Integer> bannedYears);
 }
