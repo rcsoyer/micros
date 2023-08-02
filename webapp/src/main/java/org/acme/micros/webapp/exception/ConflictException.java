a4d5eed3dbd2a28d3b92b9b5a9bd99047461941f
Rafa Soyer <rcsoyer@gmail.com>
2023-08-02 13:16:00 +0200

    refactoring - small improvement make use of improvements in serialization

--- webapp/src/main/java/org/acme/micros/webapp/exception/ConflictException.java
+++ webapp/src/main/java/org/acme/micros/webapp/exception/ConflictException.java
@@ -1,11 +1,16 @@
 package org.acme.micros.webapp.exception;
 
+import java.io.Serial;
+
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 import static org.springframework.http.HttpStatus.CONFLICT;
 
 @ResponseStatus(CONFLICT)
 public class ConflictException extends IllegalArgumentException {
+
+    @Serial
+    private static final long serialVersionUID = 5765712549838931386L;
 
     public ConflictException(final String errorMessage) {
         super(errorMessage);
