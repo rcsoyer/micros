1ae0fb152fbc492412d146f8f3b6344823a79bfc
Rafa Soyer <rcsoyer@gmail.com>
2023-08-02 12:52:04 +0200

    add another level of error handling for exceptional cases

--- webapp/src/main/java/org/acme/micros/webapp/web/errorhandling/ExceptionHandlerAdvice.java
+++ webapp/src/main/java/org/acme/micros/webapp/web/errorhandling/ExceptionHandlerAdvice.java
@@ -1,8 +1,42 @@
 package org.acme.micros.webapp.web.errorhandling;
 
+import java.sql.SQLIntegrityConstraintViolationException;
+
+import lombok.extern.slf4j.Slf4j;
+import org.hibernate.exception.ConstraintViolationException;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.zalando.problem.Problem;
+import org.zalando.problem.Status;
 import org.zalando.problem.spring.web.advice.ProblemHandling;
 
+import static org.springframework.http.HttpStatus.CONFLICT;
+
+@Slf4j
 @RestControllerAdvice
-public class ExceptionHandlerAdvice implements ProblemHandling {
+class ExceptionHandlerAdvice implements ProblemHandling {
+
+    /**
+     * Handles exceptions related to data integrity violations that may happen concurrently.
+     * <br/> Mainly is expected here to catch attempts to duplicate data on insert or update operations.
+     */
+    @ExceptionHandler({
+      ConstraintViolationException.class,
+      DataIntegrityViolationException.class,
+      SQLIntegrityConstraintViolationException.class
+    })
+    ResponseEntity<Problem> dataIntegrityViolationHandler(final Exception error) {
+        log.warn("There was an attempt to create or update data that violates integrity constraints", error);
+        final var problem = Problem.builder()
+                                   .withCause(toProblem(error))
+                                   .withStatus(Status.CONFLICT)
+                                   .withDetail("Invalid duplicated data provided")
+                                   .build();
+        return ResponseEntity
+                 .status(CONFLICT)
+                 .body(problem);
+    }
+
 }
