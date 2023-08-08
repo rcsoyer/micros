8a425fa830b5b86a980ef45f908f54e5adcfc4aa
Rafa Soyer <rcsoyer@gmail.com>
2023-08-08 13:33:22 +0200

    enable spring security again but allow the endpoints to be accessed by anyone just to make it simpler without the need of an authenticated user for now

--- webapp/src/main/java/org/acme/micros/webapp/configuration/AuditConfig.java
+++ webapp/src/main/java/org/acme/micros/webapp/configuration/AuditConfig.java
@@ -1,21 +1,31 @@
 package org.acme.micros.webapp.configuration;
 
 //its useful for reference. But in here disabling it since it would demand to configure a properly secure access
+
+import java.util.Optional;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
 /**
  * Configuration for auditing JPA entities
  */
-/*@Configuration
+@Configuration
 @EnableJpaAuditing
-public class AuditConfig {
+class AuditConfig {
 
-    *//***
+    /**
      * The user that is logged in the app when creating or modifying resources
-     *//*
+     */
     @Bean
-    public AuditorAware<String> auditorProvider() {
+    AuditorAware<String> auditorProvider() {
         return () -> Optional
                        .ofNullable(SecurityContextHolder.getContext().getAuthentication())
                        .map(Authentication::getName);
     }
 
-}*/
+}
