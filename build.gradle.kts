1d1a9824223eee6d5b526c9de1279de164f13df8
Rafa Soyer <rcsoyer@gmail.com>
2024-01-07 16:42:20 +0100

    upgrade Java to latest LTS, Gradle and all dependencies to latest stable released versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,22 +1,20 @@
 plugins {
-    id("org.springframework.boot") version "3.1.2"
-    id("io.spring.dependency-management") version "1.1.2"
-    id("io.freefair.lombok") version "8.1.0"
+    id("org.springframework.boot") version "3.2.1"
+    id("io.spring.dependency-management") version "1.1.4"
+    id("io.freefair.lombok") version "8.4"
 }
 
 val mapStructVersion by extra { "1.5.5.Final" }
-val jacksonDataTypeVersion by extra { "2.15.2" }
-val resilience4JVersion by extra { "3.0.3" }
-val feignOkHttpVersion by extra { "12.4" }
-val caffeineVersion by extra { "3.1.6" }
-val picocliSpringBootVersion by extra { "4.7.4" }
-val springCloudDepsVersion by extra { "2020.0.4" }
+val resilience4JVersion by extra { "3.1.0" }
+val feignOkHttpVersion by extra { "13.1" }
+val caffeineVersion by extra { "3.1.8" }
+val picocliSpringBootVersion by extra { "4.7.5" }
 val zalandoProblemVersion by extra { "0.29.1" }
 val springDocApiVersion by extra { "1.7.0" }
 val logBackEncoderVersion by extra { "7.4" }
-val testContainersVersion by extra { "1.18.3" }
-val eurekaVersion by extra { "4.0.3" }
-val feignVersion by extra { "4.0.4" }
+val testContainersVersion by extra { "1.19.3" }
+val eurekaVersion by extra { "4.1.0" }
+val feignVersion by extra { "4.1.0" }
 
 subprojects {
     apply(plugin = "org.springframework.boot")
--- buildSrc/src/main/kotlin/org.acme.micros.java-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/org.acme.micros.java-conventions.gradle.kts
@@ -15,7 +15,7 @@ repositories {
 
 group = "org.acme.micros"
 version = "1.0-SNAPSHOT"
-java.sourceCompatibility = JavaVersion.VERSION_17
+java.sourceCompatibility = JavaVersion.VERSION_21
 
 tasks.withType<JavaCompile>() {
     options.encoding = "UTF-8"
